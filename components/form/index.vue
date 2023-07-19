2749d3ad1155522ad10e01c97c5dfd97053a6122
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-19 15:21:55 -0300

    temp: create form component

---
+++ components/form/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>login</h1>
+
+        <input type="email" placeholder="E-mail ou usuário" />
+
+        <button>Continuar</button>
+
+        <p>ou</p>
+
+        <FormGroupButton />
+
+        <p>ou</p>
+
+        <tr />
+
+        <div>
+            <p>Termos e privacidade</p>
+            <p>Português (Brasil)</p>
+        </div>
+    </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped></style>
