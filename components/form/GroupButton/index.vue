356abe52b3355ee890f878686747e98deaef4a9b
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-19 15:22:54 -0300

    temp: create button group component

---
+++ components/form/GroupButton/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button>Google</button>
+        <button>Facebook</button>
+        <button>Apple</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ButtonGroup',
+};
+</script>
+
+<style lang="scss" scoped></style>
