6fbf228b241852990831fe5ea6fa7a8d9c2ea6a6
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-19 11:53:11 -0300

    feat: create index page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="pageStyles">
+        <h1>ola mundo</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    .pageStyles {
+        background-color: red;
+    }
+
+</style>
