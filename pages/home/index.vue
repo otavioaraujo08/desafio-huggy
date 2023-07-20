a3bcf5df53699193029e63156cfd4dd6ac3be336
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-20 16:24:28 -0300

    temp: add chat components to page

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>Home</div>
+    <div class="container">
+        <div class="chatListBox"><HomeChatList /></div>
+        <div class="chatHistoryBox">Historico das conversas</div>
+    </div>
 </template>
 
 <script></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+}
+
+.chatListBox {
+    width: 30%;
+    background: #ffffff;
+}
+
+.chatHistoryBox {
+    width: 70%;
+}
+</style>
