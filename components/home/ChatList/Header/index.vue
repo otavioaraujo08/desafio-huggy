bcf76ba3084ab243d93a16258b7f15a7553ad9a7
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-20 16:26:07 -0300

    feat: create create chat list header

---
+++ components/home/ChatList/Header/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="header">
+        <div class="headerBox">
+            <h2 class="headerTitle">Minhas mensagens</h2>
+        </div>
+    </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300&display=swap');
+
+.header,
+.headerBox {
+    display: flex;
+    align-items: center;
+    height: 5rem;
+}
+
+.header {
+    justify-content: center;
+    width: 100%;
+}
+.headerBox {
+    width: 95%;
+    border-bottom: 1px solid #e8e8eb;
+}
+
+.headerTitle {
+    margin-left: 1rem;
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #2934a5;
+    line-height: 2.25rem;
+    font-family: 'IBM Plex Sans', sans-serif;
+}
+</style>
