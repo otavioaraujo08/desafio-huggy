23f2ea710bcda7e71fd445b87e12ba3a8a6da25b
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-20 15:03:39 -0300

    fix: update responsiveness in group button component

--- components/form/GroupButton/index.vue
+++ components/form/GroupButton/index.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="buttonGroup">
         <button class="buttonLogin">
-            <Icon name="logos:google-icon" size="1.4rem" class="icon" />
+            <Icon name="logos:google-icon" class="icon" />
             Continuar com o Google
         </button>
         <button class="buttonLogin">
-            <Icon name="logos:facebook" size="1.4rem" class="icon" />
+            <Icon name="logos:facebook" class="icon" />
             Continuar com o Facebook
         </button>
         <button class="buttonLogin">
-            <Icon name="logos:apple" size="1.4rem" class="icon" />
+            <Icon name="logos:apple" class="icon" />
             Continuar com a Apple
         </button>
     </div>
@@ -25,6 +25,7 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
 
 .buttonGroup {
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -35,7 +36,7 @@ export default {
 
 .buttonLogin {
     height: 2.5rem;
-    width: 22rem;
+    width: 80%;
     background: #ffffff;
     border: 2px solid #efefef;
     border-radius: 2rem;
@@ -45,10 +46,31 @@ export default {
     font-size: 1rem;
     letter-spacing: 0.1px;
     cursor: pointer;
+
+    @media (max-width: 600px) {
+        font-size: 0.8rem;
+    }
+
+    @media (max-width: 600px) {
+        font-size: 0.6rem;
+    }
 }
 
 .icon {
     margin-right: 0.5rem;
     padding-bottom: 0.2rem;
+    height: 1.4rem;
+    width: 1.4rem;
+
+    @media (max-width: 600px) {
+        height: 1rem;
+        width: 1rem;
+    }
+
+    @media (max-width: 300px) {
+        height: 0.9rem;
+        width: 0.9rem;
+        padding-bottom: 0;
+    }
 }
 </style>
