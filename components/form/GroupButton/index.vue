ac12479c257e692776790b8d20ffa46363dbbf5f
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-20 10:58:44 -0300

    feat: update groupButtons component

--- components/form/GroupButton/index.vue
+++ components/form/GroupButton/index.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
-        <button>Google</button>
-        <button>Facebook</button>
-        <button>Apple</button>
+    <div class="buttonGroup">
+        <button class="buttonLogin">
+            <Icon name="logos:google-icon" size="1.4rem" class="icon" />
+            Continuar com o Google
+        </button>
+        <button class="buttonLogin">
+            <Icon name="logos:facebook" size="1.4rem" class="icon" />
+            Continuar com o Facebook
+        </button>
+        <button class="buttonLogin">
+            <Icon name="logos:apple" size="1.4rem" class="icon" />
+            Continuar com a Apple
+        </button>
     </div>
 </template>
 
@@ -12,4 +21,33 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
+
+.buttonGroup {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 1rem;
+}
+
+.buttonLogin {
+    height: 2.5rem;
+    width: 22rem;
+    background: #ffffff;
+    border: 2px solid #efefef;
+    border-radius: 2rem;
+    font-weight: 500;
+    font-family: 'Noto Sans', sans-serif;
+    color: #000000;
+    font-size: 1rem;
+    letter-spacing: 0.1px;
+}
+
+.icon {
+    margin-right: 0.5rem;
+    padding-bottom: 0.2rem;
+}
+</style>
