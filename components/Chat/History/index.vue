c833485e7e7fb55e2e53d2605bea09e7d66db6f6
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-21 15:17:59 -0300

    temp: create chat history component

---
+++ components/Chat/History/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-if="userSelected">
+        <div>Informações do usuário selecionado:</div>
+        <div>{{ userSelected?.name }}</div>
+        <div>{{ userSelected?.lastMessage }}</div>
+        <img :src="userSelected?.image" alt="User Image" />
+    </div>
+
+    <div v-else>Selecione um usuário</div>
+</template>
+
+<script>
+export default {
+    props: {
+        userSelected: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
