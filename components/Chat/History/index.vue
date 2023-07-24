24e2c60c9d5eab6bbf732606aefb5dcb17444a2a
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-24 18:32:53 -0300

    feat: add get messages and send messages function

--- components/Chat/History/index.vue
+++ components/Chat/History/index.vue
@@ -12,14 +12,25 @@
         </header>
 
         <body class="messageBox">
-            <div>Informações do usuário selecionado:</div>
-            <div>{{ userSelected?.lastMessage }}</div>
+            <ChatMessages :messages="historyMessages" />
         </body>
 
         <footer class="footerBox">
-            <input placeholder="Escreva sua mensagem..." class="input" />
+            <input
+                type="text"
+                @input="updateMessage"
+                v-model="message"
+                placeholder="Escreva sua mensagem..."
+                class="input"
+            />
 
-            <button class="sendButton">Enviar</button>
+            <button
+                :class="['button', message ? 'sendButton' : 'notSendButton']"
+                :disabled="!message"
+                @click="adicionarMensagem"
+            >
+                Enviar
+            </button>
         </footer>
     </div>
 
@@ -27,12 +38,77 @@
 </template>
 
 <script>
+import { chatService } from '@/services/chat';
+import { ref, watch } from 'vue';
+
 export default {
     props: {
         userSelected: {
             type: Object,
             required: true,
         },
+    },
+
+    setup(props) {
+        let historyMessages = ref([]);
+        let message = ref('');
+
+        const getMessageHistory = async () => {
+            try {
+                let response;
+
+                switch (props.userSelected.id) {
+                    case 1:
+                        response = await chatService.getIdOneMessages();
+                        break;
+                    case 2:
+                        response = await chatService.getIdTwoMessages();
+                        break;
+                    default:
+                        response = await chatService.getIdThreeMessages();
+                        break;
+                }
+
+                historyMessages.value = response;
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        const adicionarMensagem = async () => {
+            try {
+                const params = {
+                    message: message.value,
+                    date: new Date().getHours() + ':' + new Date().getMinutes(),
+                    isMine: true,
+                };
+
+                switch (props.userSelected.id) {
+                    case 1:
+                        await chatService.postIdOneMessages(params);
+                        break;
+                    case 2:
+                        await chatService.postIdTwoMessages(params);
+                        break;
+                    default:
+                        await chatService.postIdThreeMessages(params);
+                        break;
+                }
+
+                message.value = '';
+                getMessageHistory();
+            } catch (error) {
+                console.error('Erro ao adicionar nova mensagem:', error);
+            }
+        };
+
+        watch(() => props.userSelected, getMessageHistory);
+
+        return {
+            adicionarMensagem,
+            historyMessages,
+            message,
+        };
     },
 };
 </script>
@@ -70,6 +146,14 @@ export default {
     font-weight: 600;
     line-height: 160%;
     color: #141316;
+
+    @media (max-width: 800px) {
+        font-size: 0.8rem;
+    }
+
+    @media (max-width: 450px) {
+        font-size: 0.7rem;
+    }
 }
 
 .messageBox {
@@ -97,18 +181,25 @@ export default {
 .input {
     margin-top: 1rem;
     width: 65%;
-    height: 4rem;
+    height: 4.8rem;
     padding: 1rem;
     border-radius: 4px;
     box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
     border: 1px solid var(--color-border-neutral-1, #e8e8eb);
     background: var(--color-fill-neutral-low-0, #fff);
+
+    @media (max-width: 800px) {
+        width: 55%;
+    }
+
+    @media (max-width: 450px) {
+        width: 50%;
+    }
 }
 
-.sendButton {
+.button {
     width: 65%;
-    height: 3rem;
-    background: #38a96d;
+    height: 2.3rem;
     border-radius: 4px;
     border: none;
     color: #ffffff;
@@ -116,6 +207,18 @@ export default {
     font-size: 1rem;
     font-weight: 600;
     line-height: 160%;
+
+    @media (max-width: 800px) {
+        width: 55%;
+    }
+
+    @media (max-width: 450px) {
+        width: 50%;
+    }
+}
+
+.sendButton {
+    background: #38a96d;
     cursor: pointer;
 
     &:hover {
@@ -124,11 +227,16 @@ export default {
     }
 }
 
+.notSendButton {
+    background: #c2c2c2;
+    cursor: not-allowed;
+}
+
 .selectUserBox {
     height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
-    background: url('../../../public/slashes.png');
+    background: url('../../../assets/images/slashes.png');
 }
 </style>
