087409b01a53f5b42077f6e6e99145821c216466
otavioaraujo08 <otavioaraujo490@gmail.com>
2023-07-23 23:46:34 -0300

    feat: create user service

---
+++ services/user.ts
@@ -0,0 +1,10 @@
+import { api as apiService, ApiService, defaultUrl } from './api';
+
+class UserService {
+    constructor(private readonly api: ApiService) {}
+
+    public getUsers = async (): Promise<void[]> => {
+        return this.api.get(`${defaultUrl}/users`);
+    };
+}
+export const userService = new UserService(apiService);
